460321300839ccf9378c07113a1f8d64a52063b3
Alex <k.a.s.reporter@gmail.com>
2019-12-04 21:10:43 +0300

    Инфо юзеров хранится в файле

---
+++ Lab3/AdminWindow.java
@@ -0,0 +1,192 @@
+package Lab3;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import javax.swing.*;
+
+class AdminWindow extends JFrame{
+
+    private JButton addButton = new JButton("Добавить участника");
+    private JButton resButton = new JButton("Выслать результаты участникам");
+    private GridBagLayout gbl = new GridBagLayout();
+
+    AdminWindow () {
+        this.setTitle("Администратор");
+        this.setSize(300, 200);
+        this.setResizable(false);
+        Dimension sSize = Toolkit.getDefaultToolkit().getScreenSize(), fSize = getSize ();
+        if (fSize.height > sSize.height) {fSize.height = sSize.height;}
+        if (fSize.width  > sSize.width)  {fSize.width = sSize.width;}
+        setLocation ((sSize.width - fSize.width)/2, (sSize.height - fSize.height)/3);
+
+        addWindowListener(new CloseEventListener());
+
+        Container container = this.getContentPane();
+        container.setLayout(gbl);
+
+        GridBagConstraints lookCon = new GridBagConstraints();
+        GridBagConstraints addCon = new GridBagConstraints();
+
+        lookCon.gridx = 0;
+        lookCon.gridy = GridBagConstraints.RELATIVE;
+        lookCon.fill = GridBagConstraints.BOTH;
+        lookCon.insets = new Insets(10, 0, 10, 0);
+        addCon.gridx = 0;
+        addCon.gridy = GridBagConstraints.RELATIVE;
+        addCon.fill = GridBagConstraints.BOTH;
+        addCon.insets = new Insets(10, 0, 0, 0);
+
+        addButton.setFont(new Font("Times New Roman", Font.PLAIN, 14));
+        addButton.setHorizontalAlignment(JTextField.CENTER);
+        resButton.setFont(new Font("Times New Roman", Font.PLAIN, 14));
+        resButton.setHorizontalAlignment(JTextField.CENTER);
+
+        resButton.addActionListener(new ResultEventListener());
+        addButton.addActionListener(new AddUserEventListener());
+        gbl.setConstraints(resButton, lookCon);
+        gbl.setConstraints(addButton, addCon);
+
+        container.add(resButton);
+        container.add(addButton);
+        this.setVisible(true);
+    }
+
+    private static class AddUserWindow extends JFrame {
+
+        private GridBagLayout gbl = new GridBagLayout();
+        static JTextField loginInput = new JTextField("Логин");
+        static JTextField passwordInput = new JTextField("Пароль");
+        static JTextField nameInput = new JTextField("Имя");
+        private JButton addButton = new JButton("Зарегестрировать пользователя");
+        private GridBagConstraints firstInput = new GridBagConstraints();
+        private GridBagConstraints otherInput = new GridBagConstraints();
+        private GridBagConstraints button = new GridBagConstraints();
+
+        AddUserWindow() {
+            this.setTitle("Добавление пользвателя в систему");
+            this.setSize(400, 200);
+            Dimension sSize = Toolkit.getDefaultToolkit().getScreenSize(), fSize = getSize ();
+            if (fSize.height > sSize.height) {fSize.height = sSize.height;}
+            if (fSize.width  > sSize.width)  {fSize.width = sSize.width;}
+            setLocation ((sSize.width - fSize.width)/3, (sSize.height - fSize.height)/3);
+            this.addWindowListener(new UserCloseEventListener());
+
+            Container container = this.getContentPane();
+            container.setLayout(gbl);
+
+            firstInput.gridx = 0;
+            firstInput.gridy = 0;
+            firstInput.fill = GridBagConstraints.BOTH;
+            firstInput.insets = new Insets(0, 0, 10, 0);
+            otherInput.gridx = 0;
+            otherInput.gridy = GridBagConstraints.RELATIVE;
+            otherInput.fill = GridBagConstraints.BOTH;
+            otherInput.insets = new Insets(10, 0, 10, 0);
+            button.gridx = 0;
+            button.gridy = GridBagConstraints.RELATIVE;
+            button.insets = new Insets(10, 10, 10, 10);
+
+            loginInput.setFont(new Font("Times New Roman", Font.PLAIN, 14));
+            loginInput.setHorizontalAlignment(JTextField.LEFT);
+            passwordInput.setFont(new Font("Times New Roman", Font.PLAIN, 14));
+            passwordInput.setHorizontalAlignment(JTextField.LEFT);
+            nameInput.setFont(new Font("Times New Roman", Font.PLAIN, 14));
+            nameInput.setHorizontalAlignment(JTextField.LEFT);
+            addButton.setFont(new Font("Times New Roman", Font.PLAIN, 14));
+            addButton.setHorizontalAlignment(JTextField.CENTER);
+
+            gbl.setConstraints(nameInput, firstInput);
+            gbl.setConstraints(loginInput, otherInput);
+            gbl.setConstraints(passwordInput, otherInput);
+            gbl.setConstraints(addButton, button);
+
+            container.add(nameInput);
+            container.add(loginInput);
+            container.add(passwordInput);
+            addButton.addActionListener(new AddUserButtonListener());
+            container.add(addButton);
+            this.setVisible(true);
+        }
+    }
+
+    private static class AddUserButtonListener implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+            String login = AddUserWindow.loginInput.getText();
+            if (TradeSystem.findUser(login) == null) {
+                TradeSystem.addUser(AddUserWindow.nameInput.getText(), login, AddUserWindow.passwordInput.getText());
+                ObjectOutputStream out;
+                try {
+                    out = new ObjectOutputStream(new FileOutputStream("Users.bin"));
+                    for (User user1 : TradeSystem.users)
+                        out.writeObject(user1);
+                    out.close();
+                } catch (IOException ex) {
+                    System.err.println(ex.getMessage());
+                }
+            }
+            else
+                JOptionPane.showMessageDialog(null, "Такой пользователь уже зарегестрирован!",
+                        "Ошибка", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    private static class ResultEventListener implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+            TradeSystem.processRequests();
+            JOptionPane.showMessageDialog(null, "Информация по заявкам рассчитана и выслана пользователям",
+                    "", JOptionPane.INFORMATION_MESSAGE);
+        }
+    }
+
+    private static class AddUserEventListener implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+            new AddUserWindow();
+        }
+    }
+
+    private static class UserCloseEventListener implements WindowListener {
+
+        public void windowOpened(WindowEvent e) {}
+
+        public void windowClosing(WindowEvent e) {
+            AddUserWindow.nameInput.setText("Имя");
+            AddUserWindow.loginInput.setText("Логин");
+            AddUserWindow.passwordInput.setText("Пароль");
+        }
+
+        public void windowClosed(WindowEvent e) {}
+
+        public void windowIconified(WindowEvent e) {}
+
+        public void windowDeiconified(WindowEvent e) {}
+
+        public void windowActivated(WindowEvent e) {}
+
+        public void windowDeactivated(WindowEvent e) {}
+    }
+
+    static class CloseEventListener implements WindowListener {
+
+        public void windowOpened(WindowEvent e) {}
+
+        public void windowClosing(WindowEvent e) {
+            new InitialWindow();
+        }
+
+        public void windowClosed(WindowEvent e) {}
+
+        public void windowIconified(WindowEvent e) {}
+
+        public void windowDeiconified(WindowEvent e) {}
+
+        public void windowActivated(WindowEvent e) {}
+
+        public void windowDeactivated(WindowEvent e) {}
+    }
+}
---
+++ Lab3/TradeSystem.java
@@ -0,0 +1,81 @@
+package Lab3;
+
+import javax.swing.*;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.*;
+
+class TradeSystem {
+    static ArrayList<User> users = new ArrayList<>();
+    private static ArrayList<Request> requests = new ArrayList<>();
+    private static ArrayList<Request> approvedRequests = new ArrayList<>();
+    static User currentUser;
+
+    static void addUser(String name, String login, String password) {
+        users.add(new Client(name, login, password));
+    }
+
+    static void addRequest(Client requester, String product, int price, int count, int type) {
+        requests.add(new Request(requester, product, price, count, type));
+    }
+
+    static User findUser(String login) {
+        for (User user : users) {
+            if (user.getLogin().equals(login))
+                return user;
+        }
+        return null;
+    }
+
+    static void processRequests() {
+        String currentProduct;
+        int currentPrice;
+        int currentCount;
+        boolean flag = false;
+        for (int i = 0; i < requests.size(); i++) {
+            currentPrice = requests.get(i).getPrice();
+            currentCount = requests.get(i).getCount();
+            currentProduct = requests.get(i).getProduct();
+            if (requests.get(i).getType() == 0) {
+                for (Request request : requests) {
+                    if (flag) {
+                        i--;
+                        flag = false;
+                    }
+                    if (request.getProduct().equals(currentProduct) && request.getType() == 1) {
+                        if (request.getPrice() >= currentPrice) {
+                            if (currentCount - request.getCount() >= 0) {
+                                currentCount -= request.getCount();
+                                approvedRequests.add(request);
+                                requests.remove(request);
+                                if (currentCount == 0) {
+                                    approvedRequests.add(requests.get(i));
+                                    requests.remove(requests.get(i));
+                                    flag = true;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        approvedRequests.forEach(R -> R.getRequester().takeRequest(R));
+    }
+
+    public static void main(String[] args) throws IOException {
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        ObjectInputStream in = null;
+        try {
+            in = new ObjectInputStream(new FileInputStream("Users.bin"));
+            while (true)
+                users.add((User) in.readObject());
+        } catch (IOException | ClassNotFoundException e) {
+            if (in != null)
+                in.close();
+        }
+        new TradeSystem();
+        new InitialWindow();
+    }
+}
